900dfd5cc353731ece58f69e432eaea8af835665
Ashish Arora <ashish.arora@mindbowser.com>
2022-04-22 16:17:53 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+import Slider from "@mui/material/Slider";
+import { useEffect, useState } from "react";
+import { padding } from "@mui/system";
+import MuiInput from "@mui/material/Input";
+import { styled } from "@mui/material/styles";
+
+const Input = styled(MuiInput)`
+  width: 100px;
+`;
+function valuetext(value) {
+  return `${value}°C`;
+}
 
 function App() {
+  const [value, setValue] = useState(1);
+  const [price, setPrice] = useState(20000);
+  const [cal, setCal] = useState(0);
+  useEffect(() => {
+    setCal(value * (price + 2000));
+  }, [value, price]);
+
+  const handleChange = (e) => {
+    setValue(parseInt(e.target.value || 0));
+  };
+  const handlePriceChange = (e) => {
+    setPrice(parseInt(e.target.value || 0));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div style={{ display: "flex" }}>
+        <Slider
+          aria-label='Temperature'
+          defaultValue={1}
+          onChange={handleChange}
+          value={value}
+          valueLabelDisplay='auto'
+          step={1}
+          min={0}
+          max={500}
+        />
+        <Input value={value} size='small' onChange={handleChange} />
+        {/* <span style={{ paddingLeft: "10px" }}>{value}</span> */}
+      </div>
+      <div style={{ display: "flex", padding: "50px 0" }}>
+        <Slider
+          aria-label='Temperature'
+          defaultValue={20000}
+          onChange={handlePriceChange}
+          value={price}
+          valueLabelDisplay='auto'
+          step={5000}
+          min={0}
+          max={1500000}
+        />
+        <Input
+          type='number'
+          value={price}
+          size='small'
+          onChange={handlePriceChange}
+          inputProps={{
+            min: 0,
+            max: 1500000,
+            type: "number",
+            "aria-labelledby": "input-slider",
+          }}
+        />
+        {/* <span style={{ paddingLeft: "10px" }}>{price}</span> */}
+      </div>
+
+      <h1>{cal}</h1>
     </div>
   );
 }
